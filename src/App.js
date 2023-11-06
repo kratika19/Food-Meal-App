eefcdfb560f95cf0444b82081b823af808e30d2e
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2023-11-06 15:42:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Category from "./components/Category";
+import Delivery from "./components/Delivery";
+import Featured from "./components/Featured";
+import Footer from "./components/Footer";
+import Meal from "./components/Meal";
+import NewsLetter from "./components/NewsLetter";
+import TopNav from "./components/TopNav";
+import TopPicks from "./components/TopPicks";
+
+
+function App() {
+  return (
+    <div className="App">
+      <TopNav />
+      <Featured />
+      <Delivery />
+      <TopPicks />
+      <Meal/>
+      <Category/>
+      <NewsLetter />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
